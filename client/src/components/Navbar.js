2a7dbaf90d670377e0eedf52e0cae1464263b129
arpitnath <arpitnath42@gmail.com>
2020-11-29 13:14:37 +0530

    organising files

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,36 +13,38 @@ import {
 } from "./styleSheet";
 const Navbar = () => {
   return (
-    <Nav>
-      <NavbarContainer>
-        <NavLogo to="/">Re-Bank</NavLogo>
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Re-Bank</NavLogo>
 
-        <MobileIcon>
-          <FaBars />
-        </MobileIcon>
-        <NavMenu>
-          <NavItem>
-            <NavLinks to="about">About</NavLinks>
-          </NavItem>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
 
-          <NavItem>
-            <NavLinks to="discover">Discover</NavLinks>
-          </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
 
-          <NavItem>
-            <NavLinks to="services">Services</NavLinks>
-          </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
 
-          <NavItem>
-            <NavLinks to="signup">Sign Up</NavLinks>
-          </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
 
-          <NavBtn>
-            <NavBtnLink to="/sigin">Sign In</NavBtnLink>
-          </NavBtn>
-        </NavMenu>
-      </NavbarContainer>
-    </Nav>
+            <NavBtn>
+              <NavBtnLink to="/sigin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
   );
 };
 
